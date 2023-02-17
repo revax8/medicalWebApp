19400a6ad2f287032a186365b3b8b265a80f3099
Josh Yam <joshua.yam@agilethought.com>
2023-02-17 09:35:51 -0500

    remove component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 import { PatientsComponent } from './components/patients/patients.component';
-import { CreatePatientComponent } from './components/create-patient/create-patient.component';
 
 const routes: Routes = [ 
   {path: "doctors", component : DoctorsComponent},
   {path: "patients", component: PatientsComponent},
-  {path: "add", component: CreatePatientComponent}
 ];
 
 @NgModule({
