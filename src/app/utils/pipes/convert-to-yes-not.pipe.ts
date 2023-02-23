59836125c696eb9d3332db4de0da27ff0919abfc
Resendis <rezendiz@gmail.com>
2023-02-22 21:14:31 -0600

    Comments out

--- src/app/utils/pipes/convert-to-yes-not.pipe.ts
+++ src/app/utils/pipes/convert-to-yes-not.pipe.ts
@@ -8,14 +8,11 @@ export class ConvertToYesNotPipe implements PipeTransform {
   transform(value: boolean): string
    {
     if (value){
-      console.log('entro yes')
       return 'Yes';
     }
     else {
-      console.log('entro no')
       return 'No'
     }
     return '';
   }
-
 }
